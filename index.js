ff2ebde8c7e120a97ebef1c03626cc1be5c00bfa
sambarvadai <ani.csekaran@gmail.com>
2025-04-15 18:26:39 -0400

    Initial commit for backend files

---
+++ index.js
@@ -0,0 +1,35 @@
+const exp = require('express');
+const fetch = require('node-fetch');
+require('dotenv').config();
+
+const app = exp();
+const port = process.env.PORT || 3000;
+
+//Define route for OpenCage
+app.get('/api/geocode',async (req,res)=>{
+    const {q} = req.query;
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(q)}&key=${process.env.OPENCAGE_API_KEY}`;
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    }
+    catch(error){
+        console.error("Issue detected in OpenCage API")
+        res.status(500).json({error: "Server or OpenCage API error"});
+    }
+});
+app.get('/api/timezone',async(req,res)=>{
+    const {lat,lng} = req.query;
+    const url = `https://api.timezonedb.com/v2.1/get-time-zone?key=${process.env.TZDB_KEY}&format=json&by=position&lat=${lat}&lng=${lng}`;
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+        res.status(200).json(data);
+    }
+    catch(error){
+        console.error("Issue detected in timezone API");
+        res.status(500).json({error:"Server or TimezoneDB API Error"});
+    }
+    });
+    app.listen(port,()=>{console.log("Server started successfully on port",port)});
